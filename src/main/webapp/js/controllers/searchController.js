a14f276f00d7670243068f4af18cc1a67476a9e8
Angélo LIMA <alima@externe.generali.fr>
2015-03-09 13:51:08 +0100

    Remove useless angular.map

--- src/main/webapp/js/controllers/searchController.js
+++ src/main/webapp/js/controllers/searchController.js
@@ -4,6 +4,9 @@ app.controller('SearchCtrl', function ($scope, $http) {
         $http.get("/api/search?q=" + $scope.query)
             .success(function (data) {
                 $scope.cards = data;
+            })
+            .error(function (error) {
+                console.log("Error while accessing to API " + error);
             });
     }
 });
