819bd18aefb5432a9bf2d1bae5c1287a3aa00fc2
Angélo LIMA <angelomiguellima@gmail.com>
2015-03-20 22:22:54 +0100

    Fix autocompletion with accents

--- src/main/webapp/js/controllers/searchController.js
+++ src/main/webapp/js/controllers/searchController.js
@@ -5,9 +5,12 @@ app.controller('SearchCtrl', function ($scope, $http) {
     };
 
     $scope.tapedQuery = function(typed){
-        $http.get("/api/names?q=" + typed)
+        $http.get("/api/names?q=" + sansAccents(typed))
             .success(function(data) {
-                $scope.suggestions = _.map(data, _.iteratee('name'));
+                var suggestions = _.map(data, _.iteratee('name'));
+                console.log("Taped : " + sansAccents(typed));
+                console.log("Suggestions : " + suggestions);
+                $scope.suggestions = suggestions;
             });
     };
     $scope.$watch(function () { return $scope.query; }, function () {
@@ -22,6 +25,25 @@ app.controller('SearchCtrl', function ($scope, $http) {
             .error(function (error) {
                 console.log("Error while accessing to API " + error);
             });
+    };
+
+    var sansAccents = function(str){
+        var accent = [
+            /[\300-\306]/g, /[\340-\346]/g, // A, a
+            /[\310-\313]/g, /[\350-\353]/g, // E, e
+            /[\314-\317]/g, /[\354-\357]/g, // I, i
+            /[\322-\330]/g, /[\362-\370]/g, // O, o
+            /[\331-\334]/g, /[\371-\374]/g, // U, u
+            /[\321]/g, /[\361]/g, // N, n
+            /[\307]/g, /[\347]/g, // C, c
+        ];
+        var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+
+        for(var i = 0; i < accent.length; i++){
+            str = str.replace(accent[i], noaccent[i]);
+        }
+
+        return str;
     }
 
 });
