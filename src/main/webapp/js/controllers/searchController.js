011794cf3ce19eaf5ebc9a30b1cf2befa385b33c
Angélo LIMA <angelomiguellima@gmail.com>
2015-03-20 12:02:44 +0100

    Improve autocompletion

--- src/main/webapp/js/controllers/searchController.js
+++ src/main/webapp/js/controllers/searchController.js
@@ -1,22 +1,27 @@
 app.controller('SearchCtrl', function ($scope, $http) {
 
     $scope.submit = function () {
-        $http.get("/api/search?q=" + $scope.query)
+        doSearch($scope.query);
+    };
+
+    $scope.tapedQuery = function(typed){
+        $http.get("/api/names?q=" + typed)
+            .success(function(data) {
+                $scope.suggestions = _.map(data, _.iteratee('name'));
+            });
+    };
+    $scope.$watch(function () { return $scope.query; }, function () {
+       doSearch($scope.query);
+    });
+
+    var doSearch = function(query) {
+        $http.get("/api/search?q=" + query)
             .success(function (data) {
                 $scope.cards = data;
             })
             .error(function (error) {
                 console.log("Error while accessing to API " + error);
             });
-    };
-
-    $scope.tapedQuery = function(typed){
-        // MovieRetriever could be some service returning a promise
-        $http.get("/api/names?q=" + typed)
-            .success(function(data) {
-                $scope.suggestions = _.map(data, _.iteratee('name'));
-            });
-    };
-
+    }
 
 });
