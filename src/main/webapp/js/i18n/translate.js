2fccbe0d86a06197df6f0cbc2c05a9a07b0355bd
Angélo LIMA <alima@externe.generali.fr>
2015-04-09 11:41:57 +0200

    Update template

--- src/main/webapp/js/i18n/translate.js
+++ src/main/webapp/js/i18n/translate.js
@@ -6,7 +6,8 @@ app.config(function ($translateProvider) {
         CITATION: 'Citation',
         RARITY : "Rarity",
         LEGAL : "Card names and text are all copyright Blizzard Entertainment. This website is not affiliated with Blizzard Entertainment in any way.",
-        EXPANSION : "Expansion"
+        EXPANSION : "Expansion",
+        PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock legendary, ..."
     });
     $translateProvider.translations('fr', {
         CLASS: 'Classe',
@@ -15,7 +16,8 @@ app.config(function ($translateProvider) {
         CITATION: 'Citation',
         RARITY : "Rareté",
         LEGAL : "Les noms et les textes des cartes sont la propriété de Blizzard Entertainment. Ce site n'est pas affilié à Blizzard Enternainment en aucun cas.",
-        EXPANSION : "Extension"
+        EXPANSION : "Extension",
+        PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock légendaire, ..."
     });
 
     var defaultLanguage = function () {
