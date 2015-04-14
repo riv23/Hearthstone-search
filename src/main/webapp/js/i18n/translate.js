a09969eabef41effe75370cebff4099186d97bb4
Angélo LIMA <alima@externe.generali.fr>
2015-04-14 17:14:47 +0200

    Translated github page

--- src/main/webapp/js/i18n/translate.js
+++ src/main/webapp/js/i18n/translate.js
@@ -7,7 +7,8 @@ app.config(function ($translateProvider) {
         RARITY : "Rarity",
         LEGAL : "Card names and text are all copyright Blizzard Entertainment. This website is not affiliated with Blizzard Entertainment in any way.",
         EXPANSION : "Expansion",
-        PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock legendary, ..."
+        PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock legendary, ...",
+        GITHUB : "GitHub page, pull requests are welcome :)"
     });
     $translateProvider.translations('fr', {
         CLASS: 'Classe',
@@ -17,7 +18,8 @@ app.config(function ($translateProvider) {
         RARITY : "Rareté",
         LEGAL : "Les noms et les textes des cartes sont la propriété de Blizzard Entertainment. Ce site n'est pas affilié à Blizzard Enternainment en aucun cas.",
         EXPANSION : "Extension",
-        PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock légendaire, ..."
+        PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock légendaire, ...",
+        GITHUB : "Page GitHub, vos pull requests sont les bienvenues :)"
     });
 
     var defaultLanguage = function () {
