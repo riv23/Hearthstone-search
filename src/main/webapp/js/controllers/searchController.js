d01552ae47b40666a7d87c21947127e137af0dda
Angélo LIMA <alima@externe.generali.fr>
2015-04-27 09:55:42 +0200

    Add mana filter

--- src/main/webapp/js/controllers/searchController.js
+++ src/main/webapp/js/controllers/searchController.js
@@ -21,6 +21,24 @@ app.controller('SearchCtrl', function ($scope, $http, $location, NamesSrvc, Sear
             });
     };
 
+    $scope.filterMana= function(value) {
+
+        SearchSrvc.fetch($scope.query, $scope.lang)
+            .success(function (data) {
+                $scope.cards = _.filter(data, function(card){
+                    if(value != 10) {
+                        return card.cost == value;
+                    } else {
+                        return card.cost >= 10;
+                    }
+                });
+            })
+            .error(function (error) {
+                console.log("Error while accessing to API " + error);
+            });
+
+    };
+
     $scope.submit = function () {
         SearchSrvc.fetch($scope.query, $scope.lang)
             .success(function (data) {
