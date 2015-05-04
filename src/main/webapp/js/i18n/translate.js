101708ed9650de6860d282bc506d54d21754f4d6
Angélo LIMA <angelomiguellima@gmail.com>
2015-05-04 13:53:37 +0200

    add missing translation

--- src/main/webapp/js/i18n/translate.js
+++ src/main/webapp/js/i18n/translate.js
@@ -8,7 +8,8 @@ app.config(function ($translateProvider) {
         LEGAL : "Card names and text are all copyright Blizzard Entertainment. This website is not affiliated with Blizzard Entertainment in any way.",
         EXPANSION : "Expansion",
         PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock legendary, ...",
-        GITHUB : "GitHub page, pull requests are welcome :)"
+        GITHUB : "GitHub page, pull requests are welcome :)",
+        CARDS : "Card(s)"
     });
     $translateProvider.translations('fr', {
         CLASS: 'Classe',
@@ -19,7 +20,8 @@ app.config(function ($translateProvider) {
         LEGAL : "Les noms et les textes des cartes sont la propriété de Blizzard Entertainment. Ce site n'est pas affilié à Blizzard Enternainment en aucun cas.",
         EXPANSION : "Extension",
         PLACEHOLDER : "Ex : Leeroy Jenkins, Blackrock légendaire, ...",
-        GITHUB : "Page GitHub, vos pull requests sont les bienvenues :)"
+        GITHUB : "Page GitHub, vos pull requests sont les bienvenues :)",
+        CARDS : "Carte(s)"
     });
 
     var defaultLanguage = function () {
